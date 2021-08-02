3157543edaab94de4a734788ae4c7165c1f0afc2
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-08-02 12:49:25 +0300

    Another test #2

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -4,7 +4,7 @@ import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
-import io.restassured.specification.*;
+import io.restassured.specification.RequestSpecification;
 import lombok.Data;
 
 import java.util.Locale;
