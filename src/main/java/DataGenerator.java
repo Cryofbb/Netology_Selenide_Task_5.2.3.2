fa944fa8168b2c1fe1eb95eb08d8f3504155ae10
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-08-02 11:16:29 +0300

    Initial commit

---
+++ src/main/java/DataGenerator.java
@@ -0,0 +1,60 @@
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Data;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    @Data
+    public static class User {
+        private final String login;
+        private final String password;
+        private final String status;
+    }
+    public static class Registration{
+        private Registration(){
+        }
+        public static User getUser(String status) {
+            String login = loginGen();
+            String password = passGen();
+            var user = new User(login, password, status);
+            return user;
+        }
+
+        public static User getRegisteredUser(String status) {
+            var registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+    }
+    public static void sendRequest(){
+        given()
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new User("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+    }
+    public static String loginGen(){
+        String login = faker.pokemon().name();
+        return login;
+    }
+    public static String passGen(){
+        String password = faker.internet().password();
+        return password;
+    }
+}
